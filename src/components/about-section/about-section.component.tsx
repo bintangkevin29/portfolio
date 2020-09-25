ffa2debc52dcb0dcc63eefa0d32c9ba5090d3457
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-25 21:13:46 +0700

    added timeline table

--- src/components/about-section/about-section.component.tsx
+++ src/components/about-section/about-section.component.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
+import { Table } from "react-bootstrap";
 import {
   FaAngular,
   FaCss3Alt,
@@ -27,6 +28,10 @@ import CustomSection from "../custom-section";
 import SectionTitle from "../section-title";
 
 import "./about-section.style.scss";
+
+const TimelineBar: React.FC = ({ children }) => (
+  <div className="about-section__timeline-bar">{children}</div>
+);
 
 const AboutSection: React.FC = () => {
   const [about, setAbout] = useState<About>();
@@ -71,6 +76,58 @@ const AboutSection: React.FC = () => {
         <SiAdobexd className="about-section__skill-icon" />
       </div>
       <div className="about-section__about">{about?.about}</div>
+      <div className="about-section__timeline-table-container">
+        <Table borderless>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td colSpan={2}>
+                <TimelineBar>
+                  <div className="about-section__timeline-title">Davis Materialworks</div>
+                  <div className="about-section__timeline-subtitle">Full Stack Engineer</div>
+                </TimelineBar>
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td colSpan={3}>
+                <TimelineBar>
+                  <div className="about-section__timeline-title">PT Poda Husari Digital</div>
+                  <div className="about-section__timeline-subtitle">Full Stack Engineer</div>
+                </TimelineBar>
+              </td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td colSpan={5}>
+                <TimelineBar>
+                  <div className="about-section__timeline-title"> University of North Sumatra</div>
+                  <div className="about-section__timeline-subtitle">
+                    Bachelor of Computer Science (3.12 GPA)
+                  </div>
+                </TimelineBar>
+              </td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>2014/2015</td>
+              <td>2016</td>
+              <td>2017</td>
+              <td>2018</td>
+              <td>2019</td>
+              <td>2020</td>
+              <td>Current</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
     </CustomSection>
   );
 };
