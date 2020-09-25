30d5fb482bc4226481a72062f49ff9343c500b53
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-25 17:30:14 +0700

    added portfolio card compoinent

---
+++ src/components/portfolio-card/portfolio-card.component.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./portfolio-card.style.scss";
+
+const PortfolioCard: React.FC = () => {
+  return <div className="portfolio-card">Tess</div>;
+};
+
+export default PortfolioCard;
--- src/components/work-section/work-section.component.tsx
+++ src/components/work-section/work-section.component.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 import CustomSection from "../custom-section";
+import PortfolioCard from "../portfolio-card";
 
 import "./work-section.style.scss";
 
 const WorkSection: React.FC = () => {
   return (
     <CustomSection id="work-section" className="work-section">
-      tes
+      <Row>
+        <Col xs={12} md={6} lg={4}>
+          <PortfolioCard />
+        </Col>
+      </Row>
     </CustomSection>
   );
 };
