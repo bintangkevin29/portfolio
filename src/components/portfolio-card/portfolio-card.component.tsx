41777cc1b0fdf52fe8df803f56fe60c0f12424c8
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-25 19:09:03 +0700

    added tools and library

--- src/components/portfolio-card/portfolio-card.component.tsx
+++ src/components/portfolio-card/portfolio-card.component.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { RiGithubLine, RiLinksLine } from "react-icons/ri";
+import { RiGithubLine, RiLinksLine, RiToolsLine } from "react-icons/ri";
 import { Portfolio } from "../../global";
 import { firebaseStorage } from "../../lib/firebase";
 
@@ -31,33 +31,36 @@ const PortfolioCard: React.FC<Props> = ({ portfolioData }) => {
         )}
       </div>
       <div className="portfolio-card__content">
-          <div className="portfolio-card__details">
-            <div className="portfolio-card__title">{portfolioData.name}</div>
-            <div className="portfolio-card__description">{portfolioData.description} </div>
-          </div>
-          <div className="portfolio-card__navigation">
-            {portfolioData.githubUrl !== "-" && (
-              <a
-                href={portfolioData.githubUrl}
-                className="portfolio-card__navigation-items"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <RiGithubLine />
-              </a>
-            )}
-            {portfolioData.url !== "-" && (
-              <a
-                href={portfolioData.url}
-                className="portfolio-card__navigation-items"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <RiLinksLine />
-              </a>
-            )}
+        <div className="portfolio-card__details">
+          <div className="portfolio-card__title">{portfolioData.name}</div>
+          <div className="portfolio-card__description">{portfolioData.description} </div>
+          <div className="portfolio-card__tools">
+            <RiToolsLine /> {portfolioData.libraryFramework}
           </div>
         </div>
+        <div className="portfolio-card__navigation">
+          {portfolioData.githubUrl !== "-" && (
+            <a
+              href={portfolioData.githubUrl}
+              className="portfolio-card__navigation-items"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <RiGithubLine />
+            </a>
+          )}
+          {portfolioData.url !== "-" && (
+            <a
+              href={portfolioData.url}
+              className="portfolio-card__navigation-items"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <RiLinksLine />
+            </a>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
