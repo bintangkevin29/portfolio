7717d77681800276875f193def7e43be7eed0bd5
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-25 16:04:16 +0700

    added dumy dontnet

--- src/components/work-section/work-section.component.tsx
+++ src/components/work-section/work-section.component.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import CustomSection from "../custom-section";
 
 import "./work-section.style.scss";
 
 const WorkSection: React.FC = () => {
-  return <div className="work-section"></div>;
+  return <CustomSection className="work-section">tes</CustomSection>;
 };
 
 export default WorkSection;
--- src/pages/home-page/home-page.component.tsx
+++ src/pages/home-page/home-page.component.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import HeaderSection from "../../components/header-section";
+import WorkSection from "../../components/work-section";
 
 import "./home-page.style.scss";
 
@@ -7,6 +8,7 @@ const HomePage: React.FC = () => {
   return (
     <Fragment>
       <HeaderSection />
+      <WorkSection />
     </Fragment>
   );
 };
