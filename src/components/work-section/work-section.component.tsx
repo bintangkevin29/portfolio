010a5ded787e0c9ebf88becdd04fcca7cada6f71
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-25 16:03:21 +0700

    added work section component

---
+++ src/components/work-section/work-section.component.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./work-section.style.scss";
+
+const WorkSection: React.FC = () => {
+  return <div className="work-section"></div>;
+};
+
+export default WorkSection;
