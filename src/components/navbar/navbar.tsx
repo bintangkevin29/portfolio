c5b86b447e9508f3d24ac37577b505eba2f7db66
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-25 16:31:48 +0700

    added scroll hook to work menu

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,35 +2,46 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { CgBitbucket } from "react-icons/cg";
 import { RiLinkedinLine, RiInstagramLine, RiGithubLine, RiDribbbleLine } from "react-icons/ri";
+import { useScroll } from "../../lib/custom-hook";
 import SocialMediaIcon from "../social-media-icon/social-media-icon.component";
 import "./navbar.scss";
 
-const CustomNavbar: React.FC = () => (
-  <div className="navbar-custom">
-    <Row className="navbar-custom__inner">
-      <Col xs={10} className="navbar-custom__menu-container">
-        <div className="navbar-custom__menu-items navbar-custom__menu-items--selected">work</div>
-        <div className="navbar-custom__social-media-container">
-          <SocialMediaIcon href="https://www.linkedin.com/in/bintangkevin">
-            <RiLinkedinLine />
-          </SocialMediaIcon>
-          <SocialMediaIcon href="https://github.com/bintangkevin29">
-            <RiGithubLine />
-          </SocialMediaIcon>
-          <SocialMediaIcon href="https://github.com/bintangkevin29">
-            <CgBitbucket />
-          </SocialMediaIcon>
-          <SocialMediaIcon href="https://dribbble.com/bkevin29">
-            <RiDribbbleLine />
-          </SocialMediaIcon>
-          <SocialMediaIcon href="https://instagram.com/kevinsamosir29">
-            <RiInstagramLine />
-          </SocialMediaIcon>
-        </div>
-        <div className="navbar-custom__menu-items">about</div>
-      </Col>
-    </Row>
-  </div>
-);
+const CustomNavbar: React.FC = () => {
+  const isOnWorkSection = useScroll("#work-section");
+
+  return (
+    <div className="navbar-custom">
+      <Row className="navbar-custom__inner">
+        <Col xs={10} className="navbar-custom__menu-container">
+          <div
+            className={`navbar-custom__menu-items ${
+              isOnWorkSection ? "navbar-custom__menu-items--selected" : ""
+            }`}
+          >
+            work
+          </div>
+          <div className="navbar-custom__social-media-container">
+            <SocialMediaIcon href="https://www.linkedin.com/in/bintangkevin">
+              <RiLinkedinLine />
+            </SocialMediaIcon>
+            <SocialMediaIcon href="https://github.com/bintangkevin29">
+              <RiGithubLine />
+            </SocialMediaIcon>
+            <SocialMediaIcon href="https://github.com/bintangkevin29">
+              <CgBitbucket />
+            </SocialMediaIcon>
+            <SocialMediaIcon href="https://dribbble.com/bkevin29">
+              <RiDribbbleLine />
+            </SocialMediaIcon>
+            <SocialMediaIcon href="https://instagram.com/kevinsamosir29">
+              <RiInstagramLine />
+            </SocialMediaIcon>
+          </div>
+          <div className="navbar-custom__menu-items">about</div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default CustomNavbar;
--- src/components/work-section/work-section.component.tsx
+++ src/components/work-section/work-section.component.tsx
@@ -4,7 +4,11 @@ import CustomSection from "../custom-section";
 import "./work-section.style.scss";
 
 const WorkSection: React.FC = () => {
-  return <CustomSection className="work-section">tes</CustomSection>;
+  return (
+    <CustomSection id="work-section" className="work-section">
+      tes
+    </CustomSection>
+  );
 };
 
 export default WorkSection;
