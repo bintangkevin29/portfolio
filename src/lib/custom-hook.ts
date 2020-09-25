0045685e0d92fa70def478dfdf5b1ab2b22cd35f
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-25 22:59:26 +0700

    remove debug log

--- src/lib/custom-hook.ts
+++ src/lib/custom-hook.ts
@@ -6,11 +6,6 @@ export const useScroll = (id: string, initState: boolean = false) => {
   const isInViewport = (el: Element | null) => {
     if (el) {
       const rect = el.getBoundingClientRect();
-      if (id === "#about-section") {
-        console.log(id, rect.top, rect.bottom);
-        console.log(rect.top <= 0 && rect.bottom >= 0);
-      }
-
       return rect.top <= 0 && rect.bottom >= 0;
     }
   };
