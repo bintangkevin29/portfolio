bf2e6adaa546b7f4823e2df7028f62c7b6a5f043
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-25 19:10:13 +0700

    added about sectin cpomonent

---
+++ src/components/about-section/about-section.component.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./about-section.style.scss";
+
+const AboutSection: React.FC = () => {
+  return <div className="about-section"></div>;
+};
+
+export default AboutSection;
