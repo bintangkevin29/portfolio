c5c095c1c1406cbcb02c04abdf3d711732ae653e
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-26 23:43:35 +0700

    Updates

---
+++ precache-manifest.88e24643f2e63db4a0228cd6d06910d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acfaa44429f5608b05d1e52f9b273e37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9a5e6a21a63c3729a02",
+    "url": "/static/css/main.99c2fe1c.chunk.css"
+  },
+  {
+    "revision": "2291b9592c43589c5316",
+    "url": "/static/js/2.be6d1258.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/static/js/2.be6d1258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9a5e6a21a63c3729a02",
+    "url": "/static/js/main.80b6c5c9.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
